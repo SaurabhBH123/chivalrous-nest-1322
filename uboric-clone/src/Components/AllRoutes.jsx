dee3d1274dc06e5d10ad402594e4347f35f8f760
Saurabh Bhandari <bhandarisaurabh143@gmail.com>
2022-11-11 18:37:55 +0530

    added cart and checkout

--- uboric-clone/src/Components/AllRoutes.jsx
+++ uboric-clone/src/Components/AllRoutes.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import {Routes,Route} from "react-router-dom"
 import Home from '../Pages/Home'
 import Shop from "../Pages/Shop"
-// import ProductDetails from "./ProductDetails"
+import ProductDetails from "../Pages/ProductDetails"
 import SignIn from '../Pages/SignIn'
 import Cart from "../Pages/Cart"
 import AboutUs from '../Pages/AboutUs'
-// import Checkout from './Checkout'
+import Checkout from '../Pages/Checkout'
 import ContactUs from '../Pages/ContactUs'
 import WishList from '../Pages/WishList'
 import SignUp from '../Pages/SignUp'
-// import ReqAuth from "../components/Authentication/ReqAuth"
+import ReqAuth from "../Components/Authentication/ReqAuth"
 
 
 const AllRoutes = () => {
@@ -18,12 +18,12 @@ const AllRoutes = () => {
     <Routes>
         <Route path='/' element={ <Home/> } />
         <Route path='/shop' element={<Shop/> } />
-        {/* <Route path='/shop/:id' element={<ProductDetails/> } /> */}
+        <Route path='/shop/:id' element={<ProductDetails/> } />
         <Route path='/about-us' element={<AboutUs/>} />
         <Route path='/contact-us' element={<ContactUs/>} />
-        <Route path='/cart' element={<Cart/>} />
-        <Route path='/wishlist' element={<WishList/>} />
-        {/* <Route path='/checkout' element={<Checkout/>} /> */}
+        <Route path='/cart' element={<Cart/> } />
+        <Route path='/wishlist' element={<ReqAuth><WishList/></ReqAuth> } />
+        <Route path='/checkout' element={<Checkout/>} />
         <Route path='/sign-in' element={<SignIn/>} />
         <Route path='/sign-up' element={<SignUp/>} />
         <Route path='*' element={<div style={{padding:"200px"}}>This page does not exist :((</div>} />
